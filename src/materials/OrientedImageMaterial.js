b02bc70f16af17c1f19b4a9e2a7fff123a8b381b
nosy-b <ADevaux@ign.fr>
2019-11-04 17:35:04 +0100

    Corrected Shader OrientedImageMaterial without modifying chunks

--- src/materials/OrientedImageMaterial.js
+++ src/materials/OrientedImageMaterial.js
@@ -15,6 +15,109 @@ function popUniform(options, property, defaultValue) {
     return new Uniform(value);
 }
 
+
+
+var vertexShaderOrientedMaterial = `
+
+  uniform float size;
+  uniform float scale;
+
+  #ifdef USE_MAP4
+  varying highp vec3 vPosition;
+  #undef USE_MAP
+  #endif
+  ${ShaderChunk.common}
+  ${ShaderChunk.color_pars_vertex}
+  ${ShaderChunk.fog_pars_vertex}
+  ${ShaderChunk.morphtarget_pars_vertex}
+  ${ShaderChunk.logdepthbuf_pars_vertex}
+  ${ShaderChunk.clipping_planes_pars_vertex}
+
+
+
+  void main() {
+    ${ShaderChunk.color_vertex}
+    ${ShaderChunk.begin_vertex}
+    ${ShaderChunk.morphtarget_vertex}
+    ${ShaderChunk.project_vertex}
+
+    gl_PointSize = size;
+
+    ${ShaderChunk.logdepthbuf_vertex}
+    ${ShaderChunk.clipping_planes_pars_vertex}
+    ${ShaderChunk.worldpos_vertex}
+    ${ShaderChunk.fog_vertex}
+    
+
+    #ifdef USE_MAP4
+      vPosition = transformed;
+    #endif
+
+  }
+  `;
+
+
+var fragmentShaderOrientedMaterial = `
+  ${ShaderChunk.common}
+  ${ShaderChunk.color_pars_fragment}
+  ${RadialDistortion.chunks.radial_pars_fragment}
+  
+  uniform bool diffuseColorGrey;
+  #ifdef USE_MAP4
+  
+    uniform mat4 modelMatrix;
+    uniform vec3 uvwPosition;
+    uniform mat4 uvwPreTransform;
+    uniform mat4 uvwPostTransform;
+    uniform RadialDistortion uvDistortion;
+    uniform sampler2D map;
+    uniform float borderSharpness;
+    uniform float debugOpacity;
+  #endif
+
+  #ifdef USE_MAP4
+  varying highp vec3 vPosition;
+  #undef USE_MAP
+  #endif
+
+  void main() {
+
+     vec4 diffuseColor = vec4(1.,0.,0.,1.);
+
+      if (diffuseColorGrey) {
+
+        diffuseColor.rgb = vec3(dot(diffuseColor.rgb, vec3(0.333333)));
+      }
+    #ifdef USE_MAP4
+      // "uvwPreTransform * m" is equal to "camera.preProjectionMatrix * camera.matrixWorldInverse * modelMatrix"
+      // but more stable when both the texturing and viewing cameras have large coordinate values
+      mat4 m = modelMatrix;
+      m[3].xyz -= uvwPosition;
+      vec4 uvw = uvwPreTransform * m * vec4(vPosition, 1.);
+      if( uvw.w > 0. && distort_radial(uvw, uvDistortion))
+      {
+        uvw = uvwPostTransform * uvw;
+        uvw.xyz /= 2. * uvw.w;
+        uvw.xyz += vec3(0.5);
+        vec3 border = min(uvw.xyz, 1. - uvw.xyz);
+        if (all(greaterThan(border,vec3(0.))))
+        {
+          vec4 color = texture2D(map, uvw.xy);
+          color.a *= min(1., borderSharpness*min(border.x, border.y));
+          diffuseColor.rgb = mix(diffuseColor.rgb, color.rgb, color.a);
+        } else {
+          diffuseColor.rgb = mix(diffuseColor.rgb, fract(uvw.xyz), debugOpacity);
+        }
+      }
+      gl_FragColor = diffuseColor;
+    #endif
+    }
+  `;
+
+
+
+
+/*
 ShaderChunk.common = `
 ${ShaderChunk.common}
 #ifdef USE_MAP4
@@ -75,6 +178,7 @@ ${ShaderChunk.color_fragment}
 
 #endif
 `;
+*/
 
 function definePropertyUniform(object, property, defaultValue) {
     object.uniforms[property] = new Uniform(object[property] || defaultValue);
@@ -91,6 +195,7 @@ function definePropertyUniform(object, property, defaultValue) {
 
 class OrientedImageMaterial extends ShaderMaterial {
     constructor(options = {}) {
+
         const size = pop(options, 'size', 1);
         const diffuse = pop(options, 'diffuse', new Color(0xeeeeee));
         const uvwPosition = pop(options, 'uvwPosition', new Vector3());
@@ -103,8 +208,8 @@ class OrientedImageMaterial extends ShaderMaterial {
         const borderSharpness = pop(options, 'borderSharpness', 10000);
         const diffuseColorGrey = pop(options, 'diffuseColorGrey', true);
         const debugOpacity = pop(options, 'debugOpacity', 0);
-        options.vertexShader = options.vertexShader || ShaderLib.points.vertexShader;
-        options.fragmentShader = options.fragmentShader || ShaderLib.points.fragmentShader;
+        options.vertexShader = vertexShaderOrientedMaterial; //options.vertexShader || ShaderLib.points.vertexShader;
+        options.fragmentShader = fragmentShaderOrientedMaterial; //options.fragmentShader || ShaderLib.points.fragmentShader;
         options.defines = options.defines || {};
         if (map) {
             options.defines.USE_MAP4 = '';
