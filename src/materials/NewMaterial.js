338f956bcd97b5ff909b4f9591cebd8ac7de932a
mbredif <mathieu.bredif@ign.fr>
2021-05-18 18:18:25 +0200

    add Material.js

--- src/materials/NewMaterial.js
+++ src/materials/NewMaterial.js
@@ -1,35 +1,7 @@
-import { Uniform, ShaderMaterial, Matrix4, Vector3, Vector4 } from 'three';
-import { default as RadialDistortion } from '../cameras/distortions/RadialDistortion';
+import { ShaderMaterial, Matrix4, Vector3, Vector4 } from 'three';
+import { pop, definePropertyUniform, textureMatrix } from './Material.js';
 import NewMaterialVS from './shaders/NewMaterialVS.glsl';
 import NewMaterialFS from './shaders/NewMaterialFS.glsl';
-
-function pop(options, property, defaultValue) {
-    if (options[property] === undefined) return defaultValue;
-    const value = options[property];
-    delete options[property];
-    return value;
-}
-
-function definePropertyUniform(object, property, defaultValue) {
-    object.uniforms[property] = new Uniform(object[property] || defaultValue);
-    Object.defineProperty(object, property, {
-        get: () => object.uniforms[property].value,
-        set: (value) => {
-            if (object.uniforms[property].value != value) {
-                object.uniformsNeedUpdate = true;
-                object.uniforms[property].value = value;
-            }
-        }
-    });
-}
-
-// maps [-1,1]^3 to [0,1]^3
-const textureMatrix = new Matrix4().set(
-    1, 0, 0, 1,
-    0, 1, 0, 1,
-    0, 0, 1, 1,
-    0, 0, 0, 2);
-
 
 class NewMaterial extends ShaderMaterial {
   constructor(options = {}) {
--- src/materials/OrientedImageMaterial.js
+++ src/materials/OrientedImageMaterial.js
@@ -1,40 +1,7 @@
 import { Uniform, ShaderMaterial, Matrix4, Vector2, Vector3, Vector4, Color } from 'three';
+import { pop, definePropertyUniform, textureMatrix } from './Material.js';
 import OrientedImageMaterialVS from './shaders/OrientedImageMaterialVS.glsl';
 import OrientedImageMaterialFS from './shaders/OrientedImageMaterialFS.glsl';
-
-function pop(options, property, defaultValue) {
-    if (options[property] === undefined) return defaultValue;
-    const value = options[property];
-    delete options[property];
-    return value;
-}
-
-function popUniform(options, property, defaultValue) {
-    const value = pop(options, property, defaultValue);
-    if (options.uniforms[property])
-        return options.uniforms[property];
-    return new Uniform(value);
-}
-
-function definePropertyUniform(object, property, defaultValue) {
-    object.uniforms[property] = new Uniform(object[property] || defaultValue);
-    Object.defineProperty(object, property, {
-        get: () => object.uniforms[property].value,
-        set: (value) => {
-            if (object.uniforms[property].value != value) {
-                object.uniformsNeedUpdate = true;
-                object.uniforms[property].value = value;
-            }
-        }
-    });
-}
-
-// maps [-1,1]^3 to [0,1]^3
-const textureMatrix = new Matrix4().set(
-    1, 0, 0, 1,
-    0, 1, 0, 1,
-    0, 0, 1, 1,
-    0, 0, 0, 2);
 
 class OrientedImageMaterial extends ShaderMaterial {
     constructor(options = {}) {
