2a588e54148ee61c8883c884c127960e597bf2b7
Scarlleth Castro <scarllethcastro@gmail.com>
2021-06-17 16:44:01 +0200

    Start of transition of cameras selection logic to inside the material

--- src/materials/MultiTextureSpriteMaterial.js
+++ src/materials/MultiTextureSpriteMaterial.js
@@ -1,14 +1,4 @@
-import {
-  Uniform,
-  ShaderMaterial,
-  ShaderChunk,
-  DataTexture2DArray,
-  Texture, Vector2,
-  Vector3,
-  Vector4,
-  Matrix3,
-  Matrix4
-} from 'three';
+import { Uniform, ShaderMaterial, ShaderChunk, Vector2, Vector3, Vector4, Matrix3, Matrix4 } from 'three';
 import { definePropertyUniform, textureMatrix, unrollLoops } from './Material.js';
 import MultiTextureSpriteMaterialVS from './shaders/MultiTextureSpriteMaterialVS.glsl';
 import MultiTextureSpriteMaterialFS from './shaders/MultiTextureSpriteMaterialFS.glsl';
@@ -60,6 +50,7 @@ class MultiTextureSpriteMaterial extends ShaderMaterial {
     this.MAX_TEXTURES = options.MAX_TEXTURES || 40;
     this.nbTexturesUsed = 0;
     this.textureNameToIndex = {};
+    this.allCameras = [];
 
     const whiteData = new Uint8Array(3);
     whiteData.set([255, 255, 255]);
@@ -80,7 +71,9 @@ class MultiTextureSpriteMaterial extends ShaderMaterial {
             E_prime: new Vector3(),
             M_prime_Pre: new Matrix3(),
             M_prime_Post: new Matrix3(),
-            uvDistortion: { C: new THREE.Vector2(), R: new THREE.Vector4() }
+            uvDistortion: { C: new THREE.Vector2(), R: new THREE.Vector4() },
+            index: -1,
+            weight: 0
           };
           this.textureCameras[i].uvDistortion.R.w = Infinity;
           this.depthMaps[i] = this.defaultDepthMap;
@@ -96,7 +89,8 @@ class MultiTextureSpriteMaterial extends ShaderMaterial {
     this.fragmentShader = unrollLoops(MultiTextureSpriteMaterialFS, this.defines);
   }
 
-  setCamera(camera, index) {
+  setCamera(cameraObj, index) {
+      let camera = cameraObj.cam;
       camera.getWorldPosition(this.textureCameras[index].position);
       this.textureCameras[index].preTransform.copy(camera.matrixWorldInverse);
       this.textureCameras[index].preTransform.setPosition(0, 0, 0);
@@ -122,6 +116,9 @@ class MultiTextureSpriteMaterial extends ShaderMaterial {
           this.textureCameras[index].uvDistortion = { C: new THREE.Vector2(), R: new THREE.Vector4() };
           this.textureCameras[index].uvDistortion.R.w = Infinity;
       }
+
+      this.textureCameras[index].index = cameraObj.index;
+      this.textureCameras[index].weight = cameraObj.weight;
   }
 
   setTextureCameras(cameras, mapsIndexes, cameraWeights) {
@@ -133,11 +130,126 @@ class MultiTextureSpriteMaterial extends ShaderMaterial {
       console.error('cameras.length, mapsIndexes.length and cameraWeights.length must coincide in function MultiTextureSpriteMaterial.setTextureCameras().');
     }
     for (let i = 0; i < numCameras; i++) {
-      this.setCamera(cameras[i], i);
+      this.setCamera( { cam: cameras[i], index: this.textureNameToIndex[cameras[i].name], weight: cameraWeights[i] }, i );
       this.depthMaps[i] = cameras[i].renderTarget.depthTexture;
     }
     this.textureIndexes = mapsIndexes;
     this.textureWeights = cameraWeights;
+  }
+
+  createImageData(texture) {
+    var canvas2d = document.getElementById('canvas2d'),
+    ctx = canvas2d.getContext('2d');
+    const w = texture.image.width;
+    const h = texture.image.height;
+    canvas2d.width = w;
+    canvas2d.height = h;
+    ctx.save();
+    ctx.scale(1, -1);
+    ctx.drawImage(texture.image, 0, 0, w, h*-1); // draw the im
+    ctx.restore();
+    texture.image = ctx.getImageData(0, 0, w, h);
+    texture.flipY = false;
+  }
+
+  copyTexture(texture, texture2DArray, index, renderer) {
+      const width = texture2DArray.image.width;
+      const height = texture2DArray.image.height;
+      if (width != texture.image.width || height != texture.image.height) {
+          console.error("texture and texture2DArray dimensions width and height don't match.");
+          return;
+      }
+          
+      const position = new THREE.Vector3( 0, 0, index );
+      const box = new THREE.Box3( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( width-1, height-1, 0 ) );
+      renderer.copyTextureToTexture3D(box, position, texture, texture2DArray);
+  }
+
+  decreasingFunction(d) {
+    const sigma = 0.5;
+    return 1./(sigma*sigma + d*d);
+  }
+
+  updateWeights(mainCamera) {
+
+    let viewPosition = new THREE.Vector3();
+    mainCamera.getWorldPosition(viewPosition);
+    console.log('viewPosition: ', viewPosition);
+    const nbCamerasLoaded = this.allCameras.length;
+
+    let cameraDistanceArray = [];
+    for (let i = 0; i < nbCamerasLoaded; i++) {
+        let textureCameraPosition = new THREE.Vector3();
+        this.allCameras[i].cam.getWorldPosition(textureCameraPosition);
+        cameraDistanceArray[i] = [ this.allCameras[i].cam.name, viewPosition.distanceTo(textureCameraPosition) ];
+    }
+
+    cameraDistanceArray.sort( function(a, b) {return a[1] - b[1]} );
+    console.log('cameraDistanceArray sorted:\n', cameraDistanceArray);
+
+    const k = this.defines.NUM_TEXTURES;
+    // Prevention in the case when k > nbCamerasLoaded
+    const d_kplus1 = this.decreasingFunction(cameraDistanceArray[ ((k + 1) > nbCamerasLoaded) ? (nbCamerasLoaded - 1) : k ][1]);
+
+    // for (let i = 0; i < k; i++) {
+    //     //kCameras.push(cameraDistanceArray[i % nbCamerasLoaded][0]);
+
+    //     const d_i = this.decreasingFunction(cameraDistanceArray[i % nbCamerasLoaded][1]);
+        
+    //     kWeights.push(d_i - d_kplus1);
+    // }
+
+    for (let i = 0; i < nbCamerasLoaded; i++) {
+      let cameraName = this.allCameras[i].cam.name;
+      let cameraDistance = (cameraDistanceArray.find((pair) => pair[0] == cameraName))[1];
+      let d_i = this.decreasingFunction(cameraDistance);
+      this.allCameras[i].weight = d_i - d_kplus1;
+    }
+  }
+
+  newSetTextureCameras(camera, texture, renderer) {
+    // Add this camera to allCameras if it isn't already there (including texture and depthMap)
+    if (this.allCameras.find((c) => c.cam.name == camera.name) == undefined) {
+      
+      let nextIndex = this.nbTexturesUsed;  // TODO: verify that this is the same of this.allCameras.length and change it
+      
+      // Add the camera
+      this.allCameras[nextIndex] = {
+        cam: camera,
+        index: nextIndex,
+        weight: 0           // Will be recalculated just after
+      };
+
+      // Add it's texture
+      if (!texture.image.data)
+        this.createImageData(texture);
+
+      if (!this.mapArray) {
+        const width = texture.image.width;
+        const height = texture.image.height;
+        this.initializeMapArray(width, height);
+      }
+
+      this.copyTexture(texture, this.mapArray, nextIndex, renderer);
+
+      // Add it's depthMap
+      // (later when the depthMapArray works)
+
+    }
+
+    // update the weight of all cameras
+    this.updateWeights(camera);
+
+    // order them with relation to their weights
+    this.allCameras.sort( (a,b) => b.weight - a.weight );
+    console.log('allCameras inside material:\n', this.allCameras);
+
+    // // pass the best k cameras to the array textureCameras
+    // for (let i = 0; i < this.defines.NUM_TEXTURES; i++) {
+    //   this.setCamera(this.allCameras[i], i);
+    //   this.depthMaps[i] = this.allCameras[i].cam.renderTarget.depthTexture;  // TODO: try to use depthMapArray later
+    // }
+
   }
 
   setE_Primes(cameraPosition) {
@@ -236,6 +348,8 @@ struct TextureCamera {
     mat3 M_prime_Pre;
     mat3 M_prime_Post;
     RadialDistortion uvDistortion;
+    int index;
+    float weight;
 
 };
 `;
