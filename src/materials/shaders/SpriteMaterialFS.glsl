8d7ce4309db42fc2eb95d3f6ba529200cdfd8ab0
Scarlleth Castro <scarllethcastro@gmail.com>
2021-08-09 17:29:42 +0200

    Test fragCoord

--- src/materials/shaders/SpriteMaterialFS.glsl
+++ src/materials/shaders/SpriteMaterialFS.glsl
@@ -9,6 +9,7 @@ varying mat3 vH;
 varying vec4 vColor;
 varying float passShadowMapTest;
 uniform float pixelRatio;
+uniform vec2 screenSize;
 
 void main() {
   #include <logdepthbuf_fragment>
@@ -21,7 +22,7 @@ void main() {
 
   // if (passShadowMapTest > 0.5) {
 
-    vec3 texCoord = vH * vec3(gl_FragCoord.xy / pixelRatio, 1.);
+    //vec3 texCoord = vH * vec3(gl_FragCoord.xy / pixelRatio, 1.);
 
     // // Don't texture if texCoord.z < 0 (z = w in this case)
     // if (texCoord.z > 0. && distort_radial_vec3(texCoord, uvDistortion)) {
@@ -49,22 +50,32 @@ void main() {
   // } else {
   //    finalColor.rgb = vec3(0.2); // shadow color
   // }
-  finalColor.rgb = vec3(0.,0.,0.);
 
-  if (texCoord.z > 0.) {
-    finalColor.rgb = vec3(0.,1.,0.);
-  }
-  if (distort_radial_vec3(texCoord, uvDistortion)) {
-    finalColor.rgb += vec3(0.,0.,1.);
-  }
+  vec2 fragCoordCorrected = gl_FragCoord.xy / pixelRatio;
+  vec2 fragCoord = 2 * fragCoordCorrected;
+  fragCoord.x = fragCoord.x/screenSize.x;
+  fragCoord.y = fragCoord.y/screenSize.y;
+  fragCoord = fragCoord - 1.0;
 
-  texCoord = M_prime_Post * texCoord;
-  texCoord /= texCoord.z;
+  //finalColor.rgb = vec3(0.,0.,0.);
 
-  vec2 testBorder = min(texCoord.xy, 1. - texCoord.xy);
-  if (all(greaterThan(testBorder,vec2(0.)))) {
-    finalColor.rgb += vec3(0.5,0.,0.);
-  }
+  finalColor.rgb = vec3(fragCoord.x, fragCoord.y, 0.);
+
+  
+  // if (texCoord.z > 0.) {
+  //   finalColor.rgb = vec3(0.,1.,0.);
+  // }
+  // if (distort_radial_vec3(texCoord, uvDistortion)) {
+  //   finalColor.rgb += vec3(0.,0.,1.);
+  // }
+
+  // texCoord = M_prime_Post * texCoord;
+  // texCoord /= texCoord.z;
+
+  // vec2 testBorder = min(texCoord.xy, 1. - texCoord.xy);
+  // if (all(greaterThan(testBorder,vec2(0.)))) {
+  //   finalColor.rgb += vec3(0.5,0.,0.);
+  // }
 
   gl_FragColor =  finalColor;
 }
