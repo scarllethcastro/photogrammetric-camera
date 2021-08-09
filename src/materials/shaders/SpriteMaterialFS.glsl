7db5dd9d389ae0b9ac9ed85f10fb1c1a3c75e6c0
Scarlleth Castro <scarllethcastro@gmail.com>
2021-08-09 14:03:06 +0200

    Debug spriteMaterial for iTowns

--- src/materials/shaders/SpriteMaterialFS.glsl
+++ src/materials/shaders/SpriteMaterialFS.glsl
@@ -23,35 +23,47 @@ void main() {
 
     vec3 texCoord = vH * vec3(gl_FragCoord.xy / pixelRatio, 1.);
 
-    // Don't texture if texCoord.z < 0 (z = w in this case)
-    if (texCoord.z > 0. && distort_radial_vec3(texCoord, uvDistortion)) {
+    // // Don't texture if texCoord.z < 0 (z = w in this case)
+    // if (texCoord.z > 0. && distort_radial_vec3(texCoord, uvDistortion)) {
 
-      texCoord = M_prime_Post * texCoord;
-      texCoord /= texCoord.z;
+    //   texCoord = M_prime_Post * texCoord;
+    //   texCoord /= texCoord.z;
 
-      // Test if coordinates are valid, so we can texture
-      vec2 testBorder = min(texCoord.xy, 1. - texCoord.xy);
+    //   // Test if coordinates are valid, so we can texture
+    //   vec2 testBorder = min(texCoord.xy, 1. - texCoord.xy);
 
-      if (all(greaterThan(testBorder,vec2(0.))))
-      {
-        //finalColor = texture2D(map, texCoord.xy);
-        finalColor.rgb = vec3(0.,1.,0.);
-      } else {
-    	   //finalColor.rgb = vec3(0.2); // shadow color
-         finalColor.rgb = vec3(1.,0.,0.);
-      }
+    //   if (all(greaterThan(testBorder,vec2(0.))))
+    //   {
+    //     //finalColor = texture2D(map, texCoord.xy);
+    //     finalColor.rgb = vec3(0.,1.,0.);
+    //   } else {
+    // 	   //finalColor.rgb = vec3(0.2); // shadow color
+    //      finalColor.rgb = vec3(1.,0.,0.);
+    //   }
 
-    } else {
-  	   //finalColor.rgb = vec3(0.2); // shadow color
-       finalColor.rgb = vec3(0.,0.,1.);
-    }
+    // } else {
+  	//    //finalColor.rgb = vec3(0.2); // shadow color
+    //    finalColor.rgb = vec3(0.,0.,1.);
+    // }
 
   // } else {
   //    finalColor.rgb = vec3(0.2); // shadow color
   // }
+  finalColor.rgb = vec3(0.,0.,0.);
 
   if (texCoord.z > 0.) {
     finalColor.rgb = vec3(0.,1.,0.);
+  }
+  if (distort_radial_vec3(texCoord, uvDistortion)) {
+    finalColor.rgb += vec3(0.,0.,1.);
+  }
+
+  texCoord = M_prime_Post * texCoord;
+  texCoord /= texCoord.z;
+
+  vec2 testBorder = min(texCoord.xy, 1. - texCoord.xy);
+  if (all(greaterThan(testBorder,vec2(0.)))) {
+    finalColor.rgb += vec3(0.5,0.,0.);
   }
 
   gl_FragColor =  finalColor;
